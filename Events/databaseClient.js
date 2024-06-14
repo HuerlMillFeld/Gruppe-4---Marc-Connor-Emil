3e555e303d446ec3492213ea98b1899fc44aba4c
Wavesystem <167537451+HuerlMillFeld@users.noreply.github.com>
2024-06-14 19:22:31 +0200

    Update databaseClient.js

--- Events/databaseClient.js
+++ Events/databaseClient.js
@@ -1,13 +1,6 @@
-/* 
-- Erlaubt die Kommunikation mit einer Datenbank via SQL und JavaScript zu Schulungszwecken.
-- Verwenden Sie executeSqlQuery und insertInto.
-- Sie müssen nicht im Detail verstehen, wie der Code funktioniert. 
-*/
-
-// Ändern Sie die folgenden beiden Werte, um ihre Datenbank zu verbinden.
 const GROUP_NAME = "cl4";
 const PASSWORD = "o42rw7f4oult7403";
-
+ 
 const SERVER_URL = "https://ict-290.herokuapp.com/sql";
 const databaseClient = {
   executeSqlQuery: async (sql) => {
@@ -23,6 +16,47 @@ const databaseClient = {
           "Content-Type": "application/json",
         },
         method: "POST",
+        body: JSON.stringify(payload),
+      });
+ 
+      const result = await response.json();
+      if (result.error) {
+        console.error(result.error);
+      }
+      return result;
+    } catch (error) {
+      console.error("DB error", error);
+    }
+  },
+  /*
+    Beispiel:
+    - sqlTable: "form" // Name der Tabelle in der SQL Datenbank
+    - formData: {
+        // "email" Name der Spalte in der SQL Tabelle
+        // "emailField.value" Eingabe des Benutzers aus dem Formularfeld
+        email: emailField.value,
+      }
+     */
+  insertInto: async (sqlTable, formData) => {
+    let result = null;
+    const fields = Object.keys(formData);
+    const values = Object.values(formData);
+ 
+    const sql = `INSERT INTO ${sqlTable} (${fields.join(
+      ","
+    )}) VALUES ('${values.join("','")}')`;
+    try {
+      result = await databaseClient.executeSqlQuery(sql);
+    } catch (error) {
+      console.error("Fehler bei der Datenbank: ", error);
+    }
+    return result;
+  },
+};
+has context menu
+
+
+has context menu
         body: JSON.stringify(payload),
       });
 
